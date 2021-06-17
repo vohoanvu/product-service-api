d36e186aa8f689b849c96fdb3607ca002d3ebba2
Vu Vo <vu.vo@smartdev.com>
2021-06-17 16:20:46 +0700

    "deleted redundant connection string in DBContext"

--- Source/AllSopFoodService/Model/FoodDBContext.cs
+++ Source/AllSopFoodService/Model/FoodDBContext.cs
@@ -22,17 +22,6 @@ namespace AllSopFoodService.Model
         public virtual DbSet<CartItem> ShoppingCartItems { get; set; }
         public virtual DbSet<Promotion> CouponCodes { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#pragma warning disable CS1030 // #warning directive
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-BJU462V;Database=FoodDB;Trusted_Connection=True;");
-#pragma warning restore CS1030 // #warning directive
-            }
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
